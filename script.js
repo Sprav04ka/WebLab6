52afd25bb6a2f0622af4da6e537048d5feca6c6f
Polina <poly.nika22@gmail.com>
2024-01-08 16:32:44 +0300

    meow by myself

--- script.js
+++ script.js
@@ -2,57 +2,58 @@ var demo = new Vue({
     el: '#main',
     data: {
         searchString: "",
-
-        // Модель данных. Эти данные в другом случае передавались бы ajax'ом, но сейчас описаны прямо здесь для упрощения.
-
         articles: [
             {
-                "title": "27 сайтов с задачками для оттачивания навыков программирования",
-                "url": "https://proglib.io/p/27-puzzle-websites-to-sharpen-your-skills/",
-                "image": "https://proglib.io/wp-content/uploads/2016/10/coding-time-100x70.jpg"
+                "title": "Тест: Какой ты вид хлеба?",
+                "url":
+                    "https://quizterra.com/ru/kakoj-ty-vid-hleba",
+                "image":
+                    "https://www.gastronom.ru/binfiles/images/20221117/bd2b7cee.jpg"
             },
             {
-                "title": "Готовимся к собеседованию в Google: 8 месяцев непрерывной работы",
-                "url": "https://proglib.io/p/8-month-for-google-interview/",
-                "image": "https://proglib.io/wp-content/uploads/2017/01/0_b11a3_8ff3ef7b_XL-100x70.jpg"
+                "title": "Все квесты игры Ведьмак 3",
+                "url":
+                    "https://vedmak.fandom.com/wiki/%D0%9A%D0%B2%D0%B5%D1%81%D1%82%D1%8B_(%D0%92%D0%B5%D0%B4%D1%8C%D0%BC%D0%B0%D0%BA_3)",
+                "image":
+                    "https://img1.festima.ru/1/wGkI0oSKeCVy7v0P"
             },
             {
-                "title": "15 материалов по разработке игр",
-                "url": "https://proglib.io/p/gamedev-compilation/",
-                "image": "https://proglib.io/wp-content/uploads/2017/05/gamedev-100x70.png"
+                "title": "Эффект зловещей долины: суть и причины",
+                "url":
+                    "https://gb.ru/blog/effekt-zloveschej-doliny/",
+                "image":
+                    "https://medialeaks.ru/wp-content/uploads/2018/08/02989-597x600.jpg"
             },
             {
-                "title": "10 лучших видеокурсов для изучения Linux",
-                "url": "https://proglib.io/p/linux-courses/",
-                "image": "https://proglib.io/wp-content/uploads/2017/01/ruqlwta5wqy-100x70.jpg"
+                "title": "10 лучших чат ботов Telegram 2023",
+                "url":
+                    "https://www.darkhackerworld.com/2023/06/chatgpt-telegram-bot.html",
+                "image":
+                    "https://img.freepik.com/premium-photo/system-artificial-intelligence-chatgpt-chat-bot-ai-technology-smart-robot-ai-chat-gpt-application-software-robot-application-chat-gpt-generative-ai_771426-1569.jpg"
             },
             {
-                "title": "Учебный план по осваиванию современного JavaScript",
-                "url": "https://proglib.io/p/learning-modern-javascript/",
-                "image": "https://proglib.io/wp-content/uploads/2016/12/1raWO3dhM4jMjf9VY-kZzNg-100x70.png"
+                "title": "25 онлайн-сервисов для подбора психолога",
+                "url":
+                    "https://vc.ru/u/1167394-mariya-gackaya/584622-25-onlayn-servisov-dlya-poiska-i-podbora-psihologa",
+                "image":
+                    "https://cojo.ru/wp-content/uploads/2022/12/psikhologiia-illiustratsii-1.webp"
             }
         ]
     },
     computed: {
-        // Вычисленное свойство, которое содержит только те статьи, которые соответствуют searchString.
         filteredArticles: function () {
             var articles_array = this.articles,
                 searchString = this.searchString;
-
             if (!searchString) {
                 return articles_array;
             }
-
             searchString = searchString.trim().toLowerCase();
-
-            articles_array = articles_array.filter(function (item) {
-                if (item.title.toLowerCase().indexOf(searchString) !== -1) {
-                    return item;
-                }
-            })
-
-            // Возвращает массив с отфильтрованными данными.
-            return articles_array;;
+            articles_array =
+                articles_array.filter(function (item) {
+                    if (item.title.toLowerCase().indexOf(searchString) !== -1) {
+                        return item;
+                    }
+                })
         }
     }
 });
